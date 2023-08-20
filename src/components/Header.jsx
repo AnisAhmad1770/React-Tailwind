91aa3445ad5c3a384004a53b7c5a78acaaea7eae
Anis Ahmad <anisahmad1770@gmail.com>
2023-08-20 23:59:39 +0500

    Header1

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+
+
+const Header = () => {
+  return (
+    
+       <div className='bg-[#2699fb] p-4'>
+    <div className='max-w-[1240px] py-[15px] mt-[20px] items-center  mx-auto flex justify-between'>
+        <div>
+            Liverpool Football
+        </div>
+        <ul className='flex text-white gap-5'>
+            <li>
+                Home
+                </li>
+            <li>
+                Company
+                </li>
+            <li>
+                Resources
+                </li>
+            <li>
+                About
+                </li>
+            <li>
+                Contact
+                </li>
+        </ul>
+    </div>
+     </div>
+    
+  )
+}
+
+export default Header
